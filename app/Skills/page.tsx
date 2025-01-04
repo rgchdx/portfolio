201ef1ee0bb74d00c6a5fa1d88791f1c3881b3b4
Richie Dix <rgchdx@gmail.com>
2025-01-04 15:31:35 -0600

    Added topnav and footer to skills page

--- app/Skills/page.tsx
+++ app/Skills/page.tsx
@@ -1,8 +1,20 @@
+import Footer from '@/components/Footer'
+import TopNav from '@/components/TopNav'
+import { TracingBeam } from '@/components/ui/Tracing-Beam'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <main className='relative bg-black-100 flex justify-center 
+    items-center flex-col overflow-hidden'>
+      <TopNav />
+      <TracingBeam className="px-15">
+      <div className="max-w-7xl w-full">
+        <div>Currently Working on this</div>
+        <Footer />
+      </div>
+      </TracingBeam>
+    </main>
   )
 }
 
